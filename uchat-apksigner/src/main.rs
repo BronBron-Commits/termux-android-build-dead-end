7ca045ff1a4e17ef098c193b96834278718ff800
BronBron-Commits <brmanley@outlook.com>
2025-11-21 12:49:37 -0500

    Add tempfile dependency and implement cert.sf block generation

--- uchat-apksigner/src/main.rs
+++ uchat-apksigner/src/main.rs
@@ -1,30 +1,67 @@
-use std::fs::{File};
-use std::io::{Write};
+use std::fs::{self, File};
+use std::io::{Read, Write};
+use std::path::Path;
+use zip::{ZipArchive, ZipWriter};
 use zip::write::FileOptions;
-use zip::ZipWriter;
-use rcgen::{Certificate, CertificateParams, PKCS_RSA_SHA256};
+use tempfile::tempdir;
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    // Generate a basic self-signed certificate
-    let mut params = CertificateParams::new(vec!["uchat".into()]);
-    params.alg = &PKCS_RSA_SHA256;
-    let cert = Certificate::from_params(params)?;
+fn main() -> std::io::Result<()> {
+    let input_apk = "app-unsigned.apk";
+    let output_apk = "app-signed.apk";
 
-    let cert_der = cert.serialize_der()?;
-    let key_der = cert.serialize_private_key_der();
+    let manifest_content = "\
+Manifest-Version: 1.0
+Created-By: uchat-apksigner
+";
 
-    // Create META-INF/CERT.RSA as zip
-    let out_file = File::create("CERT.RSA")?;
-    let mut zip = ZipWriter::new(out_file);
+    let cert_sf_content = "\
+Signature-Version: 1.0
+Created-By: uchat-apksigner
 
-    let options = FileOptions::default().compression_method(zip::CompressionMethod::Stored);
+Name: res/layout/activity_main.xml
+SHA1-Digest: dummyhash1234567890==
+";
 
-    zip.start_file("CERT.DER", options)?;
-    zip.write_all(&cert_der)?;
+    let cert_rsa_content = b"DUMMY SIGNATURE BLOCK PLACEHOLDER";
 
-    zip.start_file("PRIVATEKEY.DER", options)?;
-    zip.write_all(&key_der)?;
+    let input_file = File::open(input_apk)?;
+    let mut zip_reader = ZipArchive::new(input_file)?;
+    let temp_dir = tempdir()?;
+    let temp_path = temp_dir.path().join("signed.apk");
+    let output_file = File::create(&temp_path)?;
+    let mut zip_writer = ZipWriter::new(output_file);
 
-    zip.finish()?;
+    for i in 0..zip_reader.len() {
+        let mut file = zip_reader.by_index(i)?;
+        let name = file.name().to_string();
+
+        if name.starts_with("META-INF/") {
+            continue; // Strip all old signature files
+        }
+
+        let options = FileOptions::default()
+            .compression_method(file.compression())
+            .last_modified_time(file.last_modified());
+
+        zip_writer.start_file(name.clone(), options)?;
+        let mut buffer = Vec::new();
+        file.read_to_end(&mut buffer)?;
+        zip_writer.write_all(&buffer)?;
+    }
+
+    // Add new META-INF files
+    zip_writer.start_file("META-INF/MANIFEST.MF", FileOptions::default())?;
+    zip_writer.write_all(manifest_content.as_bytes())?;
+
+    zip_writer.start_file("META-INF/CERT.SF", FileOptions::default())?;
+    zip_writer.write_all(cert_sf_content.as_bytes())?;
+
+    zip_writer.start_file("META-INF/CERT.RSA", FileOptions::default())?;
+    zip_writer.write_all(cert_rsa_content)?;
+
+    zip_writer.finish()?;
+    fs::copy(&temp_path, output_apk)?;
+    println!("Signed APK written to {}", output_apk);
+
     Ok(())
 }
